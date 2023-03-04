c1b1bd2f40ee59e07951c8e885962b5f000dc54c
Oleksandr Tolochko <0504Fusion@gmail.com>
2023-03-04 19:30:43 +0200

    del
    delete comments ...

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
 import users from './DB/users.json'
 import { Card } from "./components/Card/Card"
 import { Grid, Box} from "@mui/material"
 import { useState } from 'react'
-
 
 function App() {
     const [followed, setFollowed] = useState(JSON.parse(window.localStorage.getItem('followed')) || [])
@@ -11,6 +9,7 @@ function App() {
     const onClick = (id) => {
 
         const copyFollowed = [...followed]
+
         if(copyFollowed.includes(id)) {
             const index = copyFollowed.indexOf(id)
             copyFollowed.splice(index, 1)
@@ -24,7 +23,6 @@ function App() {
         window.localStorage.setItem('followed', JSON.stringify(copyFollowed))
     }
 
-
     return (
         <Box sx={{ flexGrow: 1, padding: '15px', background: 'linear-gradient(to right, #3c1053, #ad5389)' }}>    
         <Grid container rowSpacing={4} columnSpacing={2} sx={{ padding: '15px' }}>
@@ -34,7 +32,6 @@ function App() {
         </Grid>
     </Box>
     )
-
 }
 
 export default App;
